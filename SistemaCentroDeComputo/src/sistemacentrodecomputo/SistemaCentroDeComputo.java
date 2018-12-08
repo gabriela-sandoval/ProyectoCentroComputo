c0bb004bb2eb204def5292ddfa3a5393cf740195
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2018-12-08 16:28:14 -0600

    Interfaz "Iniciar sesión"
    Diseño de la interfaz

--- SistemaCentroDeComputo/src/sistemacentrodecomputo/SistemaCentroDeComputo.java
+++ SistemaCentroDeComputo/src/sistemacentrodecomputo/SistemaCentroDeComputo.java
@@ -5,29 +5,37 @@
  */
 package sistemacentrodecomputo;
 
+import java.io.IOException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
  *
- * @author galil
+ * @author Gabriela Sandoval Cruz
  */
 public class SistemaCentroDeComputo extends Application {
   
   @Override
-  public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-    
+  public void start(Stage primaryStage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource(
+        "/interfazGrafica/FxmlSistemaCentroDeComputo.fxml"));
+
     Scene scene = new Scene(root);
-    
-    stage.setScene(scene);
-    stage.show();
+
+    primaryStage.setScene(scene);
+    Image icono = new Image("/iconos/logo cc.png");
+    primaryStage.getIcons().add(icono);
+    primaryStage.setResizable(false);
+    primaryStage.show();
   }
 
   /**
+   * Clase main de AnalisisEstiloJava.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
