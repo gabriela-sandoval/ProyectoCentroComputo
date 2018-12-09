92613eb27d631d688b856affa4fbe8cdc7853115
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2018-12-08 20:56:08 -0600

    Modificación de "Iniciar sesion"
    Se modificó la ventana "Iniciar sesión"

--- SistemaCentroDeComputo/src/sistemacentrodecomputo/SistemaCentroDeComputo.java
+++ SistemaCentroDeComputo/src/sistemacentrodecomputo/SistemaCentroDeComputo.java
@@ -1,35 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemacentrodecomputo;
 
+import java.io.IOException;
 import javafx.application.Application;
-    import javafx.fxml.FXMLLoader;
-    import javafx.scene.Scene;
-    import javafx.scene.layout.VBox;
-    import javafx.stage.Stage;
-    import java.net.URL;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.scene.control.TextInputControl;
 
 /**
- *
+ * Clase principal de.
+ * 
  * @author Gabriela Sandoval Cruz
+ * @version 1.0
+ * @since 2018-12-08
  */
 public class SistemaCentroDeComputo extends Application {
 
   @Override
-  public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FxmlSistemaCentroDeComputo.fxml"));        
-        Scene scene = new Scene(root);        
-        stage.setScene(scene);
-        stage.show();
-    }
+  public void start(Stage primaryStage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource(
+        "/interfazGrafica/FxmlSistemaCentroDeComputo.fxml"));
+
+    Scene scene = new Scene(root);
+
+    primaryStage.setScene(scene);
+    Image icono = new Image("/iconos/logo cc.png");
+    primaryStage.getIcons().add(icono);
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
 
   /**
    * Clase main de AnalisisEstiloJava.
-   *
    * @param args the command line arguments
    */
   public static void main(String[] args) {
